2e5fd73798e812ef9d788f9becbd267ec6f9c037
Rishi Narang <rishinarang@Rishis-MacBook-Pro.local>
2015-04-04 20:07:22 +0530

    asd

--- koa/website/routes/questionRoutes.js
+++ koa/website/routes/questionRoutes.js
@@ -1,12 +1,46 @@
-var render = require("./../lib/render.js")
-
+var render = require("./../lib/render.js");
+var parse = require("co-body");
 var db = require("./../lib/db.js");
+var utils = require("./../lib/utils.js");
 
-module.exports.newQuestion = function*(){
+module.exports.showNewQuestion = function *() {
+	this.body = yield render("newQuestion");
+};
 
-	var newQuestion = yield db.questions.find({});
+module.exports.addQuestion = function * () {
+	var postedData = yield parse(this);
 
+	var questionToStore  =  {
+		title : postedData.questionTitle,
+		tags : utils.splitAndTrimTagString(postedData.tagString)
+	};
 
+	var q = yield db.questions.insert(questionToStore);
 
-	this.body = yield render("newQuestion", {questions: newQuestion})
-}
+	this.redirect("/question/" + q._id);
+};
+
+module.exports.showQuestion = function *(id) {
+	var q = yield db.questions.findById(id);
+
+	var vm = {
+		id : q._id.toString(),
+		questionTitle : q.title,
+		tagString : q.tags.join(", ")
+	};
+
+	this.body = yield render("showQuestion", vm);
+};
+
+module.exports.updateQuestion = function *(id) {
+	var postedData = yield parse(this);
+
+	var questionToStore  =  {
+		title : postedData.questionTitle,
+		tags : utils.splitAndTrimTagString(postedData.tagString)
+	};
+
+	yield db.questions.updateById(id, questionToStore);
+
+	this.redirect("/question/" + id);
+};
